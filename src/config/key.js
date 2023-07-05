7109b1852a2f8a5cda112b398698e93842271825
michacp <michacp@hotmail.com>
2023-07-05 16:00:23 -0500

    cambio 1.5

--- src/config/key.js
+++ src/config/key.js
@@ -2,8 +2,12 @@
 
 
 module.exports = {
-    key:{
+    key1:{
         key:'src/keys/jwtRS256.key',//"./keys/jwtRS256.key");
         public:'src/keys/jwtRS256.key.pub'//("./keys/jwtRS256.key.pub");
+    },
+    key:{
+        key:'./keys/jwtRS256.key',
+        public:'./keys/jwtRS256.key.pub'
     }
 }
