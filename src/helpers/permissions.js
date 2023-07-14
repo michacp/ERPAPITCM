ce3f9f8c502ad0a77c7592f7f327c6b73d4341b4
michacp <michacp@hotmail.com>
2023-07-14 11:11:34 -0500

    cambio 1.5

--- src/helpers/permissions.js
+++ src/helpers/permissions.js
@@ -6,7 +6,7 @@ const {key}=require('../config/key')
 
 permissions.isadmin =async (req, res, next) => {
  
-    var cert = fs.readFileSync("src/keys/jwtRS256.key.pub")//("./keys/jwtRS256.key.pub");
+    var cert = fs.readFileSync(key.public)//("./keys/jwtRS256.key.pub");
     //console.log("aqui")
     jwt.verify(req.token, cert,async (error, authData) => {
         if (error) {
